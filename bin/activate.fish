51c2e0f6f4506bee6629af53ee7e941305c5a39b
sury <408334011@qq.com>
2015-07-12 16:22:39 +0800

    new

--- bin/activate.fish
+++ bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/vendream/classmonitor"
+set -gx VIRTUAL_ENV "/home/sury/classmonitor"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH

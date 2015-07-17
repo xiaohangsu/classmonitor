0a3c9cf0dd818bbabd58229d28d463384ba05656
kiadragon <sxhdragon@gmail.com>
2015-07-17 15:14:07 +0800

    sendEmail

--- sendEmail.py
+++ sendEmail.py
@@ -4,12 +4,11 @@
 from db import newdb, userdb
 from tools import EmailSender, HTMLgenerator
 
-def sendEmail():
-    users = userdb.getAllUserInfo()
-    userList = users["userList"]
-    userCount = users["userCount"]
-    for user in userList:
-        for catalog in user["subscribe"]:
-            print newdb.get({"newCatalog": catalog})
-
-sendEmail()
+def sendEmail(email, subscribe):
+    result = False
+    newsList = []
+    for s in subscribe: 
+        newsList.append(newdb.get({'newCatalog': s})["news"])
+    print "send"
+    result = EmailSender.send_mail("中国好班长订阅内容", HTMLgenerator.generateForSendEmail(newsList), [email])
+    return {"result": result}

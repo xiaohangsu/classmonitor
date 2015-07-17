0704e7948cb507f28bf84dffbced83cf5ac980cf
kiadragon <sxhdragon@gmail.com>
2015-07-17 15:13:14 +0800

    git ignore &lib update

--- lib/python2.7/tools/HTMLgenerator.py
+++ lib/python2.7/tools/HTMLgenerator.py
@@ -3,7 +3,9 @@
 #bulid a beautified HTML in HTMLgenerator
 
 from db import userdb
-
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 header = "<div style='background: #fff; width: 600px; padding: 20px 30px;\
 margin: 0 auto;'><div style='padding: 15px 0;'><table border='0' cellspacing='0' cellpadding='0'><tbody>"
 footer = "</tbody>\
@@ -18,10 +20,9 @@ footer = "</tbody>\
 def generate(updatedList, url=""):
     html = ""
     currentCatalog = ""
-    count = 0
     for eOld in updatedList:
         e = eval(eOld)
-        count = count + 1
+        print e
         if currentCatalog != e["newCatalog"]:
 
             currentCatalog = e["newCatalog"]
@@ -30,11 +31,24 @@ def generate(updatedList, url=""):
         html +=  ("<tr><td style='border-bottom: 1px dotted #ccc; padding: 15px 0;'><a style='text-decoration: none; font: 14px Helvetica, Arial, sans-serif; color: #0088e7;' href='" + \
             url + e["newHref"] + "' target='_blank'>" + e["newTitle"] + "</a><p style='font: 14px Helvetica, Arial, sans-serif; color: #888; padding: 15px 0 0; margin: 0; line-height: 1.6;'><span style='border-bottom:1px dashed #ccc;' t='5' times=''>" + \
             e["newTime"] + "</span></p></td></tr>")
-        if count == 2:
-            break
     html =  header + html + footer
     
-    f = open("1.html", "wb")
-    f.write(html[0:-1])
-    f.close()
+    return html
+
+def generateForSendEmail(updatedList, url=""):
+    html = ""
+    for catalogNews in updatedList:
+
+        if catalogNews[0] != "" :
+            html += ("<td><h3 style='height: 30px; color: #943907; width: 250px; margin: 0; padding: 15px 0 2px; border-bottom: 1px solid #943907; line-height: 30px; font-size: 18px;'>" + \
+                catalogNews[0]["newCatalog"].encode("utf-8") + "</h3></td>")
+            for e in catalogNews:
+                print e["newTime"]
+                html +=  ("<tr><td style='border-bottom: 1px dotted #ccc; padding: 15px 0;'><a style='text-decoration: none; font: 14px Helvetica, Arial, sans-serif; color: #0088e7;' href='" + \
+                    url + e["newHref"] + "' target='_blank'>" + e["newTitle"] + "</a><p style='font: 14px Helvetica, Arial, sans-serif; color: #888; padding: 15px 0 0; margin: 0; line-height: 1.6;'><span style='border-bottom:1px dashed #ccc;' t='5' times=''>" + \
+                    (e["newTime"]).encode("utf-8") + "</span></p></td></tr>")
+        else:
+            continue
+    html =  header + html + footer
+    print "html"
     return html

f6c516170a580a5be8a349139e8d7e5fbe3a25a9
kiadragon <sxhdragon@gmail.com>
2015-07-17 15:17:15 +0800

    update new crawl Spider pipeline

--- scrapy_jwcSYSU/scrapy_jwcSYSU/pipelines.py
+++ scrapy_jwcSYSU/scrapy_jwcSYSU/pipelines.py
@@ -9,46 +9,42 @@ import json
 from scrapy.contrib.exporter import JsonItemExporter
 from db import newdb
 
-import HTMLgenerator
-import EmailSender
+from tools import EmailSender, HTMLgenerator
+from newsCatalog import NEWSCATALOG
 
 class jwcsysuJsonExportPipeline(object):
     def __init__(self):
         self.news = set()
         self.updatedNews = set()
-        self.currentCatalog = ""
-        self.currentCatalogList = []
 
+        self.newsList = []
         f = open("data.json")
         for line in f:
             self.news.add(line[0:-1])
 
         f.close()
         self.file = open("data.json", "wb")
+        for s in NEWSCATALOG["data"][0]["list"]:
+            newdb.update({'newCatalog': s, 'newsList': []})
+
 
     def process_item(self, item, spider):
         self.updatedNews.add(json.dumps(dict(item), ensure_ascii=False, sort_keys=True).encode('utf-8'))
         line = json.dumps(dict(item), ensure_ascii=False, sort_keys=True).encode('utf-8') + "\n"
-        
-        if self.currentCatalog != item["newCatalog"] and self.currentCatalog != "":
-            newdb.update({'newCatalog': self.currentCatalog, 'newsList': self.currentCatalogList})
-            self.currentCatalogList = []
-        else :
-            self.currentCatalogList.append(dict(item))
-        self.currentCatalog = item["newCatalog"]
-
+        self.newsList.append(json.dumps(dict(item), ensure_ascii=False, sort_keys=True).encode('utf-8'))
         self.file.write(line)
         return item
 
     def close_spider(self, spider):
         
         print "Here is Updated News !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n"
-        
+        for new in self.newsList:
+            newdb.add(new)
         # 去重发送邮件
         self.updatedNews -= self.news
         
         self.updatedNews = list(self.updatedNews)
-        #print list(self.updatedNews)
+
         self.updatedNews.sort()
         EmailSender.send_mail("SpiderEmailTest", HTMLgenerator.generate(self.updatedNews))
         self.file.close()
